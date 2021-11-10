5745fdf43ff31021feaa4d772cff9872821a6b39
Hannah Hill <hannahhill@hannahs-mbp-3.home>
2021-11-10 16:51:16 +0000

    app.js switch

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// import Home from './pages/Home.js'
 
 const App = () => {
   useEffect(() => {
@@ -15,7 +17,17 @@ const App = () => {
   }, [])
 
   return (
-    <h1> Hello World</h1>
+    <Router>
+      <header>
+        <h1> Hello World</h1>
+        <Nav />
+      </header>
+      <main>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </main>
+    </Router>
   )
 }
 
