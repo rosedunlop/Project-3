1a27c96f1fdc9475cabc69148d055508c479f343
rosedunlop <rosedunlop@outlook.com>
2021-11-12 12:40:45 +0000

    modals created for login and register

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from './Nav.js'
 import OffcanvasNav from './Offcanvas.js'
+import Button from 'react-bootstrap/Button'
+import ModalRegister from './ModalRegister.js'
+import ModalLogin from './ModalLogin.js'
+
 
 const Header = () => {
+  
+  const [showLogin, setShowLogin] = useState(false)
+
+  const handleClose = () => setShowLogin(false)
+  const handleShow = () => setShowLogin(true)
+
+  const [showRegister, setShowRegister] = useState(false)
+
+  const handleCloseRegister = () => setShowRegister(false)
+  const handleShowRegister = () => setShowRegister(true)
+
+
+
   return (
     <>
       <header>
@@ -15,8 +32,12 @@ const Header = () => {
             <h1>aioli</h1>
           </div>
           <div className='login-container'>
-            <button className='auth-button login'>Login</button>
-            <button className='auth-button'> Register</button>
+            <Button className='auth-button login' variant="primary" onClick={handleShow}>Login</Button>
+            <ModalLogin showLogin={showLogin} handleClose={handleClose}/>
+
+            <Button className='auth-button' onClick={handleShowRegister}> Register</Button>
+            <ModalRegister showRegister={showRegister} handleCloseRegister={handleCloseRegister} />
+
           </div>
         </div>
       </header>
