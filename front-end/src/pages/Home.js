34aa628aca9b251a02f3abb5c2837aa34799ad64
rosedunlop <rosedunlop@outlook.com>
2021-11-17 14:51:35 +0000

    finished carousel

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import RecipeHomeCard from '../components/RecipeHomeCard.js'
+import Carousel  from 'react-bootstrap/Carousel'
+import { Link } from 'react-router-dom'
+
 
 const Home = () => {
   const [recipes, setRecipes] = useState([])
@@ -24,17 +27,66 @@ const Home = () => {
   return (
     <>
       {recipes.length && (
-        <>
-          <div className='main-recipe-container'>
-            <div className='description'>
-              <h1>{recipes[2].title.slice(0, 24)}<span>{recipes[2].title.slice(-13)}</span></h1>
-              <p>{recipes[2].description}</p>
-              <button className='button'>+</button>
-            </div>
-            <div className='image'>
-              <img src={recipes[2].image} alt='recipe-image' />
-            </div>
+      <>
+        <Carousel className="carousel">
+        <Carousel.Item className="item">
+        <div className="container">  
+        <div className="img">
+         <img
+          className="first-image"
+          src={recipes[2].image}
+          alt="First slide"
+         />
+         </div>
+        <div className="caption">
+          <h1>{recipes[2].title}</h1>
+          <p>{recipes[2].description}</p>
+          <div className="link">
+         <Link to={`/recipes/${recipes[2]._id}`}>View recipe</Link>
+          <button className='button'>+</button>
+         </div>
+        </div>
+        </div>
+        </Carousel.Item>
+        <Carousel.Item className="item">
+         <div className='container'> 
+         <div className="img"> 
+         <img
+          className="first-image"
+          src={recipes[4].image}
+          alt="Second slide"
+        />
+        </div>
+       <div className="caption">
+         <h1>{recipes[4].title}</h1>
+         <p>{recipes[4].description}</p>
+         <div className="link">
+         <Link to={`/recipes/${recipes[4]._id}`}>View recipe</Link>
+         <button className='button'>+</button>
+         </div>
+       </div>
+       </div> 
+       </Carousel.Item>
+       <Carousel.Item className="item">
+         <div className="container">
+         <div className="img">
+          <img
+           className="first-image"
+           src={recipes[10].image}
+           alt="Third slide"
+          />
+         </div> 
+        <div className="caption">
+           <h1>{recipes[10].title}</h1>
+           <p>{recipes[10].description}</p>
+           <div className="link">
+           <Link to={`/recipes/${recipes[10]._id}`}>View recipe</Link>
+           <button className='button'>+</button>
           </div>
+        </div>
+        </div>
+        </Carousel.Item>
+        </Carousel>
           <div className='top-recipes'>
             <h4 className='heading-recipe'>This Weeks Top Recipes</h4>
             <div className='recipe-card'>
